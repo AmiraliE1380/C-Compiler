36236b800aef868f5b57e0c4050277e69069ae65
amirali <ebrahimzadeh.amirali@gmail.com>
2023-03-17 19:46:14 +0330

    Implemented few utility functions.

--- compiler.py
+++ compiler.py
@@ -6,5 +6,6 @@ Emad Zinoghli - 98103267
 import scanner
 
 
+print('hi\nhi\nhi'.split('\n'))
 input_prog = open('input.txt', 'r').read()
 scanner.scanner_run(input_prog)
--- scanner.py
+++ scanner.py
@@ -3,19 +3,73 @@ errors = []
 lexemes = []
 
 
-def get_next_token(chars):
+def clear(num, line):
+    for _ in range(num):
+        line.pop()
+
+
+def is_NUM(chars):
     pass
+
+
+def is_ID(chars):
+    pass
+
+
+def is_KEYWORD(chars):
+    pass
+
+
+def is_SYMBOL(chars):
+    pass
+
+
+def is_COMMENT(chars):
+    pass
+
+
+def is_WHITESPACE(chars):
+    pass
+
+
+def get_next_token(chars):
+    if is_WHITESPACE(chars):
+        return
+    if is_COMMENT(chars):
+        return
+    if is_NUM(chars):
+        return
+    if is_ID(chars):
+        return
+    if is_KEYWORD(chars):
+        return
+    if is_SYMBOL(chars):
+        return
+
+
 
 
 def write_files():
     pass
 
 
-def scanner_run(input_prog):
-    chars = list(input_prog)    # will produce list of characters
-    print(chars)
+def get_lines(input_prog):
+    lines = []
+    line = []
+    for char in input_prog:
+        if char == '\n':
+            lines.append(line)
+            line = []
+        else:
+            line.append(char)
+    return lines
 
-    while len(chars) > 0:
-        get_next_token(chars)
+
+def scanner_run(input_prog):
+    lines = get_lines(list(input_prog))
+    print(lines)
+    for line in lines:
+        while len(line) > 0:
+            get_next_token(line)
 
     write_files()
